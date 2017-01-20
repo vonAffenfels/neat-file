9cd0d34db5c463e734035194daec7d00a35fadbb
Dominik Kienzler <dominik.kienzler@vonaffenfels.de>
2017-01-20 15:24:52 +0100

    distribute location corrected

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ module.exports = class Files extends Module {
     static defaultConfig() {
         return {
             uploadDir: "/data/uploads",
+            imagesDir: "/data/images",
             fileDir: "/data/files",
             dbModuleName: "database",
             uploadRoute: "/upload",
@@ -76,12 +77,12 @@ module.exports = class Files extends Module {
                     } else {
                         this.distributor = new Distributor({
                             debug: this.config.distributeDebug || false,
-                            root: Application.config.root_path,
+                            root: Application.config.root_path + this.config.fileDir,
                             servers: conf[this.config.distributeKey].servers
                         });
                         this.distributorGenerated = new Distributor({
                             debug: this.config.distributeDebug || false,
-                            root: null,
+                            root: Application.config.root_path + this.config.imagesDir,
                             servers: conf[this.config.distributeKeyGenerated].servers
                         });
                     }
@@ -211,11 +212,11 @@ module.exports = class Files extends Module {
                         }
 
                         if (this.distributor) {
-                            return this.distributor.distributeFile(newFile.get("filepath")).then(() => {
-                                this.log.debug("Distributed File");
+                            return this.distributor.distributeFile(newFile.get("filename")).then(() => {
+                                this.log.debug("Distributed File " + newFile.get("filename"));
                                 return resolve(newFile);
                             }, (e) => {
-                                this.log.error("Distribution of file " + newFile.get("filepath") + " failed!");
+                                this.log.error("Distribution of file " + newFile.get("filename") + " failed!");
                                 this.log.error(e);
                                 return resolve(newFile);
                             });
@@ -353,11 +354,11 @@ module.exports = class Files extends Module {
                         this.log.debug("Moved File to target Location!");
 
                         if (this.distributor) {
-                            this.distributor.distributeFile(newFile.get("filepath")).then(() => {
-                                this.log.debug("Distributed File " + newFile.get("filepath"));
+                            this.distributor.distributeFile(newFile.get("filename")).then(() => {
+                                this.log.debug("Distributed File " + newFile.get("filename"));
                                 resolve(newFile);
                             }, (e) => {
-                                this.log.error("Distribution of file " + newFile.get("filepath") + " failed!");
+                                this.log.error("Distribution of file " + newFile.get("filename") + " failed!");
                                 this.log.error(e);
                                 resolve(newFile);
                             });
